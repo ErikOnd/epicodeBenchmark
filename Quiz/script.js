dd0ddceeda21882064cf43b019e7303ea7cc1095
Erik <erik.ondra@yahoo.com>
2022-12-15 11:18:42 +0100

    implementing timer

--- Quiz/script.js
+++ Quiz/script.js
@@ -10,6 +10,7 @@ const questions = [
       "Computer Personal Unit",
       "Central Processor Unit",
     ],
+    time: 16,
   },
   {
     category: "Science: Computers",
@@ -19,6 +20,7 @@ const questions = [
       "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
+    time: 16,
   },
   {
     category: "Science: Computers",
@@ -27,6 +29,7 @@ const questions = [
     question: "The logo for Snapchat is a Bell.",
     correct_answer: "False",
     incorrect_answers: ["True"],
+    time: 16,
   },
   {
     category: "Science: Computers",
@@ -36,6 +39,7 @@ const questions = [
       "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
+    time: 6,
   },
   {
     category: "Science: Computers",
@@ -45,6 +49,7 @@ const questions = [
       "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
+    time: 6,
   },
   {
     category: "Science: Computers",
@@ -66,6 +71,7 @@ const questions = [
       "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    time: 6,
   },
   {
     category: "Science: Computers",
@@ -74,6 +80,7 @@ const questions = [
     question: "On Twitter, what is the character limit for a Tweet?",
     correct_answer: "140",
     incorrect_answers: ["120", "160", "100"],
+    time: 6,
   },
   {
     category: "Science: Computers",
@@ -82,6 +89,7 @@ const questions = [
     question: "Linux was first created as an alternative to Windows XP.",
     correct_answer: "False",
     incorrect_answers: ["True"],
+    time: 6,
   },
   {
     category: "Science: Computers",
@@ -91,6 +99,7 @@ const questions = [
       "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
+    time: 6,
   },
 ];
 
@@ -114,7 +123,128 @@ window.onload = function () {
 // If you are presenting all the questions together, just take all the radio buttons and check if the selected answer === correct_answer
 // If you are presenting one question at a time, just add one point or not to the user score if the selected answer === correct_answer
 
+let currentQuestion = 0;
 let allAnswers = [];
+let selectedAnswers = [];
+let userScore = 0;
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+let TIME_LIMIT = questions[currentQuestion].time;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+  timeLeft
+)}</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+  timePassed = -1;
+  nextQnA()
+}
+
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    console.log(timePassed, timeLeft);
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+    if (timeLeft === 0) {
+      onTimesUp();
+
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
 
 function getAllAnswers() {
   for (i = 0; i < questions.length; i++) {
@@ -137,8 +267,6 @@ function shuffleAnserts() {
     }
   }
 }
-
-let currentQuestion = 0;
 
 function displayQnA() {
   let areaForNewQuestion = document.getElementById("question-area");
@@ -172,9 +300,6 @@ function deleteQnA() {
   parent.innerHTML = "";
 }
 
-let selectedAnswers = [];
-let userScore = 0;
-
 function storeAnswers() {
   let userSelected = document.getElementsByClassName("selected");
   if (
@@ -183,7 +308,6 @@ function storeAnswers() {
   ) {
     userScore++;
   }
-  console.log(userScore);
 }
 
 function nextQnA() {
@@ -193,11 +317,14 @@ function nextQnA() {
     };
   }
 
-  localStorage.setItem("userscore", userScore);
 
+  clearInterval(timerInterval);
+  timePassed = -1;
+  localStorage.setItem("userscore", userScore);
   storeAnswers();
   deleteQnA();
   displayQnA();
+  startTimer();
 }
 
 // function unselectSelected ()
