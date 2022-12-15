8a9b65ae6e80c0dde2b6ce7fc1c5b4083562ac38
Jovellyn Quiapos <jovellynanne11@gmail.com>
2022-12-15 14:35:30 +0000

    vscode prettier fixed the layout

--- Result-page/results.js
+++ Result-page/results.js
@@ -1,31 +1,26 @@
-
-let userScore = localStorage.getItem('userscore')
+let userScore = localStorage.getItem("userscore");
 
 let userScorePercentage = userScore * 10;
 
-
 function userScorePercentageDisplay() {
-    let percentage = document.getElementsByClassName("per-heading")[0]
-    console.log(percentage)
-    percentage.innerText = userScorePercentage + "%";
+  let percentage = document.getElementsByClassName("per-heading")[0];
+  console.log(percentage);
+  percentage.innerText = userScorePercentage + "%";
 
-    let subResult = document.getElementsByClassName("sub-text")[0]
-    subResult.innerText = userScore + "/" + 10 + " questions"
-
+  let subResult = document.getElementsByClassName("sub-text")[0];
+  subResult.innerText = userScore + "/" + 10 + " questions";
 }
 
-userScorePercentageDisplay()
+userScorePercentageDisplay();
 
 function userScorePercentageWrongDisplay() {
-    let percentage = document.getElementsByClassName("per-heading")[1]
-    percentage.innerText = 100 - userScorePercentage + "%";
+  let percentage = document.getElementsByClassName("per-heading")[1];
+  percentage.innerText = 100 - userScorePercentage + "%";
 
-    let subResult = document.getElementsByClassName("sub-text")[1]
-    subResult.innerText = 10 - userScore + "/" + 10 + " questions"
+  let subResult = document.getElementsByClassName("sub-text")[1];
+  subResult.innerText = 10 - userScore + "/" + 10 + " questions";
 }
-userScorePercentageWrongDisplay()
-
-
+userScorePercentageWrongDisplay();
 
 let currentPie = `
 --p: ${userScorePercentage};
@@ -43,9 +38,9 @@ let currentPie = `
 `;
 
 function userScorePieChart() {
-    let pie = document.getElementsByClassName("pie")[0]
-    pie.style.cssText = "";
-    pie.style.cssText = currentPie;
+  let pie = document.getElementsByClassName("pie")[0];
+  pie.style.cssText = "";
+  pie.style.cssText = currentPie;
 }
 
-userScorePieChart()
+userScorePieChart();
